20cb1164c19b87150c8c5865db0ab7ff344da6b4
Sardar Shakeyev <s.shakeyev@mail.ru>
2026-01-29 20:48:16 +0500

    Update string.py

--- Practice1/python-basics/string.py
+++ Practice1/python-basics/string.py
@@ -1,1 +1,35 @@
+a = "Hello"
+print(a)
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+a = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+print(a)
+a = "Hello, World!"
+print(a[1])
+for x in "banana":
+  print(x)
+a = "Hello, World!"
+print(len(a))
+txt = "The best things in life are free!"
+print("free" in txt)
+b = "Hello, World!"
+print(b[-5:-2])
+a = "Hello, World!"
+print(a.upper())
+a = " Hello, World! "
+print(a.strip())
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
+txt = f"The price is {20 * 59} dollars"
+print(txt)
+txt = "We are the so-called \"Vikings\" from the north."
+
 
