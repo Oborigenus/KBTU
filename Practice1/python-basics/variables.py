9ef3050cba6c964580254ff0a82f7a1b7a882dde
Sardar Shakeyev <s.shakeyev@mail.ru>
2026-01-29 20:27:39 +0500

    Update variables.py

--- Practice1/python-basics/variables.py
+++ Practice1/python-basics/variables.py
@@ -1,1 +1,27 @@
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
+x = "awesome"
 
+def myfunc():
+  x = "fantastic"
+  print("Python is " + x)
+
+myfunc()
+
+print("Python is " + x)
