9e45d8e54ea059eebaf72c77cc0cac55e1b12325
Sardar Shakeyev <s.shakeyev@mail.ru>
2026-02-05 19:15:00 +0500

    Update if_elif_else.py

--- Practice2/if_else/if_elif_else.py
+++ Practice2/if_else/if_elif_else.py
@@ -1,1 +1,11 @@
+age = 25
+
+if age < 13:
+  print("You are a child")
+elif age < 20:
+  print("You are a teenager")
+elif age < 65:
+  print("You are an adult")
+elif age >= 65:
+  print("You are a senior")
 
