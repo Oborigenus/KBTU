001ad8b3edb4bb6fafd205d58164d4fdb9c2dc4b
Sardar Shakeyev <s.shakeyev@mail.ru>
2026-02-07 12:10:22 +0500

    Update if_statement.py

--- Practice2/if_else/if_statement.py
+++ Practice2/if_else/if_statement.py
@@ -2,3 +2,14 @@ a = 33
 b = 200
 if b > a:
   print("b is greater than a")
+a = 33
+b = 200
+
+if b > a:
+  pass
+age = 16
+
+if age < 18:
+  pass # TODO: Add underage logic later
+else:
+  print("Access granted")
