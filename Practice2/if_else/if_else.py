bdb832c9d04350b081f49bd05bfd318b3e4261a4
Sardar Shakeyev <s.shakeyev@mail.ru>
2026-02-07 12:09:17 +0500

    Update if_else.py

--- Practice2/if_else/if_else.py
+++ Practice2/if_else/if_else.py
@@ -4,3 +4,8 @@ if b > a:
   print("b is greater than a")
 else:
   print("b is not greater than a")
+age = 20
+if age >= 18:
+  print("You are an adult")
+  print("You can vote")
+  print("You have full legal rights")
