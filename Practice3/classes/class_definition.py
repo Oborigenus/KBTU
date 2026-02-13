37b5cd9baa440fcbf496b02207101f0d9b623146
Sardar Shakeyev <s.shakeyev@mail.ru>
2026-02-13 20:14:16 +0500

    Update class_definition.py

--- Practice3/classes/class_definition.py
+++ Practice3/classes/class_definition.py
@@ -1,1 +1,10 @@
+///
+Python is an object oriented programming language.
+
+Almost everything in Python is an object, with its properties and methods.
+
+A Class is like an object constructor, or a "blueprint" for creating objects.
+///
+class MyClass:
+  x = 5
 
