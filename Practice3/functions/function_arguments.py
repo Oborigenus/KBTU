59741eecade903527bbbc3c98407aa726f5ae5ba
Sardar Shakeyev <s.shakeyev@mail.ru>
2026-02-13 20:10:09 +0500

    Update function_arguments.py

--- Practice3/functions/function_arguments.py
+++ Practice3/functions/function_arguments.py
@@ -1,1 +1,13 @@
+def my_function(fname):
+  print(fname + " Refsnes")
 
+my_function("Emil")
+my_function("Tobias")
+my_function("Linus")
+def my_function(country = "Norway"):
+  print("I am from", country)
+
+my_function("Sweden")
+my_function("India")
+my_function()
+my_function("Brazil")
