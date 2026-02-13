0dcf61b52d0fc24310dc56aeef959d844a4d30a8
Sardar Shakeyev <s.shakeyev@mail.ru>
2026-02-13 20:14:41 +0500

    Update class_methods.py

--- Practice3/classes/class_methods.py
+++ Practice3/classes/class_methods.py
@@ -1,1 +1,20 @@
+class Person:
+  def __init__(self, name):
+    self.name = name
+
+  def greet(self):
+    print("Hello, my name is " + self.name)
+
+p1 = Person("Emil")
+p1.greet()
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def get_info(self):
+    return f"{self.name} is {self.age} years old"
+
+p1 = Person("Tobias", 28)
+print(p1.get_info())
 
