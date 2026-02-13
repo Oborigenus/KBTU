2a8e86d45b5da0c02f8f635269c5dcaa7c2c1c68
Sardar Shakeyev <s.shakeyev@mail.ru>
2026-02-13 20:09:44 +0500

    Update basic_functions.py

--- Practice3/functions/basic_functions.py
+++ Practice3/functions/basic_functions.py
@@ -1,1 +1,16 @@
+def my_function():
+  print("Hello from a function")
+calculate_sum()
+_private_function()
+myFunction2()
+temp1 = 77
+celsius1 = (temp1 - 32) * 5 / 9
+print(celsius1)
 
+temp2 = 95
+celsius2 = (temp2 - 32) * 5 / 9
+print(celsius2)
+
+temp3 = 50
+celsius3 = (temp3 - 32) * 5 / 9
+print(celsius3)
