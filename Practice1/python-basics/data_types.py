5d78e77e99a7952a717645e8fe7722547e890f0c
Sardar Shakeyev <s.shakeyev@mail.ru>
2026-03-27 21:06:01 +0500

    Add contacts.csv

--- Practice1/python-basics/data_types.py
+++ Practice1/python-basics/data_types.py
@@ -1,14 +1,14 @@
-x = str("Hello World")	str	
-x = int(20)	int	
-x = float(20.5)	float	
-x = complex(1j)	complex	
-x = list(("apple", "banana", "cherry"))	list	
-x = tuple(("apple", "banana", "cherry"))	tuple	
-x = range(6)	range	
-x = dict(name="John", age=36)	dict	
-x = set(("apple", "banana", "cherry"))	set	
-x = frozenset(("apple", "banana", "cherry"))	frozenset	
-x = bool(5)	bool	
-x = bytes(5)	bytes	
-x = bytearray(5)	bytearray	
+x = str("Hello World")
+x = int(20)
+x = float(20.5)
+x = complex(1j)
+x = list(("apple", "banana", "cherry"))	
+x = tuple(("apple", "banana", "cherry"))	
+x = range(6)
+x = dict(name="John", age=36)	
+x = set(("apple", "banana", "cherry"))	
+x = frozenset(("apple", "banana", "cherry"))
+x = bool(5)	
+x = bytes(5)
+x = bytearray(5)	
 x = memoryview(bytes(5))
