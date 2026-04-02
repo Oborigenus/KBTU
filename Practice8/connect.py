af0f4882e15fae2c7829a124291bbc06e1d5313b
Sardar Shakeyev <s.shakeyev@mail.ru>
2026-04-02 20:48:19 +0500

    Add Practice8

---
+++ Practice8/connect.py
@@ -0,0 +1,9 @@
+import psycopg2
+from config import DB_CONFIG
+
+def connect():
+    try:
+        conn = psycopg2.connect(**DB_CONFIG)
+        return conn
+    except Exception as e:
+        print("Connection error:", e)
---
+++ Practice8/phonebook.py
@@ -0,0 +1,60 @@
+from connect import connect
+
+conn = connect()
+cur = conn.cursor()
+
+def upsert(name, phone):
+    cur.execute("CALL upsert_contact(%s, %s)", (name, phone))
+    conn.commit()
+
+def search(pattern):
+    cur.execute("SELECT * FROM search_contacts(%s)", (pattern,))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+def paginate(limit, offset):
+    cur.execute("SELECT * FROM get_contacts_paginated(%s, %s)", (limit, offset))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+def delete(value):
+    cur.execute("CALL delete_contact(%s)", (value,))
+    conn.commit()
+
+def bulk_insert():
+    names = ["Alice", "Bob", "Charlie"]
+    phones = ["1234567890", "invalid", "9876543210"]
+
+    cur.execute("CALL insert_many(%s, %s)", (names, phones))
+    conn.commit()
+
+
+while True:
+    print("\n1.Insert/Update\n2.Search\n3.Paginate\n4.Delete\n5.Bulk Insert\n0.Exit")
+    choice = input("Choose: ")
+
+    if choice == "1":
+        name = input("Name: ")
+        phone = input("Phone: ")
+        upsert(name, phone)
+
+    elif choice == "2":
+        pattern = input("Search: ")
+        search(pattern)
+
+    elif choice == "3":
+        limit = int(input("Limit: "))
+        offset = int(input("Offset: "))
+        paginate(limit, offset)
+
+    elif choice == "4":
+        value = input("Name or Phone: ")
+        delete(value)
+
+    elif choice == "5":
+        bulk_insert()
+
+    elif choice == "0":
+        break
